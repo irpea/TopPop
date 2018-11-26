3ad10091f49835fd0b1f0ad0e27479553adfbc80
irpea <irpea@live.com>
2018-11-27 00:11:54 +0100

    Working Swipe to refresh!

--- app/src/main/java/com/clover/irpea/toppop/activity/ChartActivity.java
+++ app/src/main/java/com/clover/irpea/toppop/activity/ChartActivity.java
@@ -1,5 +1,6 @@
 package com.clover.irpea.toppop.activity;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -24,12 +25,34 @@ public class ChartActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private ChartAdapter adapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
 
+        swipeRefreshLayout = findViewById(R.id.swipe_container);
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        getRetrofitData();
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                getRetrofitData();
+                Log.wtf("SwipeRefresh called", "Swipe Refresh called");
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+    }
+
+    private void getRetrofitData() {
         //Handle for retrofit
         DeezerService service = RetrofitInstance.getRetrofitInstance().
                         create(DeezerService.class);
